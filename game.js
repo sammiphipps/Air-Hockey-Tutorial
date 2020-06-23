9cbc125f44dfa533da571b29304648d7df83b511
Samantha Phipps <sammi.phipps@gmail.com>
2020-06-23 17:56:34 -0400

    created Player circle setup according to the mouses x and y coordinated. The tutorial showed how to do it with a Player class, so this is the reason why I used it. In addition, the player will disappear if it goes beyond the 'table' limits.

--- game.js
+++ game.js
@@ -1,4 +1,10 @@
-const board = (ctx, w, h) => {
+const canvas = document.querySelector('#table')
+const w = canvas.width
+const h = canvas.height
+const ctx = canvas.getContext('2d')
+ctx.lineWidth = 2
+
+const board = () => {
     ctx.beginPath()
     ctx.arc(w / 2, h / 2, w / 10, 0, 2 * Math.PI)
     ctx.moveTo(0, h / 2)
@@ -6,13 +12,46 @@ const board = (ctx, w, h) => {
     ctx.stroke()
 }
 
-window.addEventListener("DOMContentLoaded", event => {
-    const canvas = document.querySelector("#table")
-    const w = canvas.width
-    const h = canvas.height
-    const ctx = canvas.getContext('2d')
-    ctx.lineWidth = 2
-    board(ctx, w, h)
+class Player {
+    constructor() {
+        this.x = undefined
+        this.y = undefined
+        this.prevX = undefined
+        this.prevY = undefined
+        this.dx = undefined
+        this.dy = undefined
+    }
+
+    draw(){
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, w * .05, 0, 2 * Math.PI)
+        ctx.fillStyle = "red"
+        ctx.fill()
+        ctx.stroke()
+    }
+
+    update() {
+        this.dx = this.x - this.prevX
+        this.dy = this.y - this.prevY
+        this.prevX = this.x
+        this.prevY = this.y
+    }
+}
+
+const player = new Player()
+
+window.addEventListener('mousemove', event => {
+    player.x = event.x - window.innerWidth / 2 + w / 2
+    player.y = event.y - h * .05
 })
+
+const animate = () => {
+    ctx.clearRect(0, 0, w, h)
+    board()
+    player.draw()
+    requestAnimationFrame(animate)
+}
+
+animate()
 
 
