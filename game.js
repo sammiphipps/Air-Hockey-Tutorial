b33c98609b1cc5843d04ac586850e9aadd4ecef1
Samantha Phipps <sammi.phipps@gmail.com>
2020-06-26 18:20:53 -0400

    Completed Disc function.

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector('#table')
 const boardWidth = canvas.width
 const boardHeight = canvas.height
+const boardCenterX = boardWidth / 2
+const boardCenterY = boardHeight / 2
 const ctx = canvas.getContext('2d')
 ctx.lineWidth = 2
 
@@ -11,75 +13,188 @@ const board = () => {
     ctx.lineTo(boardWidth, boardHeight / 2)
     ctx.stroke()
 }
-function Disc (radius, color) {
-    this.startingPosX = boardWidth / 2
-    this.startingPosY = boardHeight / 2
-    this.x = this.startingPosX
-    this.y = this.startingPosY
-    this.radius = radius
-    this.mass = 15
-    this.velocityX = 0
-    this.velocityY = 0
-    this.maxSpeed = 10
-    this.frictionX = 0.997
-    this.frictionY = 0.997
-    this.acceleration = 1
-    this.color = color
+
+function Disc () {
+
+    this.startingPosX = boardCenterX;
+    this.startingPosY = boardCenterY;
+    this.x = this.startingPosX;
+    this.y = this.startingPosY;
+    this.radius = 34;
+    this.mass = 15;
+    this.velocityX = 0;
+    this.velocityY = 0;
+    this.maxSpeed = 10;
+    this.frictionX = 0.997;
+    this.frictionY = 0.997;
+    this.acceleration = 1;
+    this.color = '#000000';
 
     this.keepControllerInBoard = () => {
-        if (this.x > (boardWidth - this.radius) || this.x < this.radius) {
-            if (this.x < this.radius) {
-                this.velocityX = 2
-            } else {
-                this.velocityX = -2
+        
+            if (this.x > (boardWidth - this.radius) || this.x < this.radius) {
+
+                if (this.x < this.radius) {
+                          this.velocityX = 2;
+                    } else {
+                            this.velocityX = -2;
+                    }
             }
-        }
 
-        if (this.y > (boardHeight - this.radius) || this.y < this.radius) {
-            if (this.y < this.radius){
-                this.velocityY = 2
-            } else {
-                this.velocityY = -2
+            if (this.y > (boardHeight - this.radius) || this.y < this.radius) {
+
+                    if (this.y < this.radius) {
+                          this.velocityY = 2;
+                    } else {
+                            this.velocityY = -2;
+                    }
+
             }
-        }
+        
+            if (controller.x > (boardCenterX - controller.radius) && controller.x < boardCenterX) {
+                    controller.velocityX = -3;
+            }
 
-        if (controller.x > ((boardWidth / 2) - controller.radius) && controller.x < (boardWidth / 2)){
-            controller.velocityX = -3
-        }
-
-        if(controllerTwo.x > (boardWidth / 2) && controllerTwo.x < ((boardWidth / 2) + (controllerTwo.radius / 2))){
-            controllerTwo.velocityX = +3
-        }
-    } 
+            if (controllerTwo.x > boardCenterX && controllerTwo.x < (boardCenterX + (controllerTwo.radius / 2))) {
+                    controllerTwo.velocityX = +3;
+            } 
+    },
 
     this.keepPuckInBoard = () => {
-        if (this.x > (boardWidth - this.radius) || this.x < this.radius) {
-            if (this.x > (boardWidth - this.radius)) {
-                this.x = boardWidth - this.radius
-            } else {
-                this.x = this.radius
+
+            if (this.x > (boardWidth - this.radius) || this.x < this.radius) {
+
+                    if (this.x > (boardWidth - this.radius)) {
+                            this.x = boardWidth - this.radius;
+                    } else {
+                            this.x = this.radius;
+                    }
+
+                    if (this.y > (goalPosTop + puck.radius) && this.y < (goalPosTop + goalHeight) - puck.radius) {
+                            puck = new Disc(boardCenterX, boardCenterY);
+                    } else {
+                            this.velocityX = -this.velocityX;
+                    }
             }
 
-            if (this.y > (goalPosTop + puck.radius) && this.y < (goalPosTop + goalHeight) - puck.radius) {
-                puck = new Disc(boardWidth / 11, 'black')
-            } else {
-                this.velocityX = -this.velocityX
-            }
-        }
+            if (this.y > (boardHeight - this.radius) || this.y < this.radius) {
 
-        if (this.y > (boardHeight - this.radius) || this.y < this.radius) {
-            if (this.y > (boardHeight - this.radius)){
-                this.y = boardHeight - this.radius
-            } else {
-                this.y = this.radius
+                    if (this.y > (boardHeight - this.radius)) {
+                            this.y = boardHeight - this.radius;
+                    } else {
+                            this.y = this.radius;
+                    }
+                    
+                    this.velocityY = -this.velocityY;
             }
 
-            this.velocityY = -this.velocityY
-        }
     }
-}
 
-const puck = new Disc(boardWidth / 11, 'black') 
+    this.discCollision = () => {
+
+            for (var i = 0; i < controllers.length; i++) {
+                
+                    var distanceX = this.x - controllers[i].x,
+                            distanceY = this.y - controllers[i].y,
+                            distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY),
+                            addedRadius = this.radius + controllers[i].radius;
+		
+                    if (distance < addedRadius) {
+                            var angle = Math.atan2(distanceY, distanceX),
+                                    sin = Math.sin(angle),
+                                    cos = Math.cos(angle),
+                                    pos0 = {
+                                            x: 0,
+                                            y: 0
+                                    },
+                                    pos1 = rotate(distanceX, distanceY, sin, cos, true),
+                                    vel0 = rotate(controllers[i].velocityX, controllers[i].velocityY, sin, cos, true),
+                                    vel1 = rotate(this.velocityX, this.velocityY, sin, cos, true),
+                                    velocityXTotal = vel0.x - vel1.x;
+
+                            vel0.x = ((controllers[i].mass - this.mass) * vel0.x + 2 * this.mass * vel1.x) /
+                                    (controllers[i].mass + this.mass);
+                            vel1.x = velocityXTotal + vel0.x;
+
+                            var absV = Math.abs(vel0.x) + Math.abs(vel1.x),
+                                    overlap = (controllers[i].radius + this.radius) - Math.abs(pos0.x - pos1.x);
+
+                            pos0.x += vel0.x / absV * overlap;
+                            pos1.x += vel1.x / absV * overlap;
+
+                            var pos0F = rotate(pos0.x, pos0.y, sin, cos, false),
+                                    pos1F = rotate(pos1.x, pos1.y, sin, cos, false);
+
+                            this.x = controllers[i].x + pos1F.x;
+                            this.y = controllers[i].y + pos1F.y;
+                            controllers[i].x = controllers[i].x + pos0F.x;
+                            controllers[i].y = controllers[i].y + pos0F.y;
+
+                            var vel0F = rotate(vel0.x, vel0.y, sin, cos, false),
+                                    vel1F = rotate(vel1.x, vel1.y, sin, cos, false);
+
+                            controllers[i].velocityX = vel0F.x;
+                            controllers[i].velocityY = vel0F.y;
+
+                            this.velocityX = vel1F.x;
+                            this.velocityY = vel1F.y;
+
+                    }
+            }
+
+    }
+
+    this.draw = () => {
+            boardContext.shadowColor = 'rgba(50, 50, 50, 0.25)';
+            boardContext.shadowOffsetX = 0;
+            boardContext.shadowOffsetY = 3;
+            boardContext.shadowBlur = 6;
+            boardContext.beginPath();
+            boardContext.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+            boardContext.fillStyle = this.color;
+            boardContext.fill();
+
+    }
+
+    this.move = () => {
+            this.velocityX *= this.frictionX;
+            this.velocityY *= this.frictionY;
+            this.x += this.velocityX;
+            this.y += this.velocityY;
+    }
+
+    this.computerPlayer = () => {        
+            if (puck.x > (boardCenterX - 30) && controllerTwo.x > (boardCenterX + controllerTwo.radius * 2)) {
+                    if ((puck.x + puck.radius) < controllerTwo.x) {
+                          controllerTwo.velocityX -= controllerTwo.acceleration;
+                    } else {
+                            controllerTwo.velocityX += controllerTwo.acceleration;
+                    }
+
+                    if (puck.y < controllerTwo.y) {
+                            controllerTwo.velocityY -= controllerTwo.acceleration;
+                    } else {
+                            controllerTwo.velocityY += controllerTwo.acceleration;
+                    }
+
+            } else {
+
+                    if (controllerTwo.x > (controllerTwo.startingPosX - 50) && controllerTwo.x < (controllerTwo.startingPosX + 50)) {
+                            controllerTwo.velocityX = 0;
+                    } else if (controllerTwo.x < (controllerTwo.startingPosX - 80)) {
+                            controllerTwo.velocityX += controllerTwo.acceleration;
+                    } else {
+                            controllerTwo.velocityX -= controllerTwo.acceleration;
+                    }
+
+            }
+
+    }
+
+};
+
+
+const puck = new Disc() 
 
 console.log(puck)
 // class Player {
